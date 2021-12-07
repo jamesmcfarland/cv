06be5e4e8a034a54d10b08baee32a6d49baf6ac5
james mcfarland <24841376+jamesmcfarland@users.noreply.github.com>
2021-12-07 03:20:44 +0000

    Quick test for MUI + to test workflow

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/700.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
